403846049104f3dae58a71d7ed583200cb679d15
jkribeiro <me@jeankribeiro.com>
2018-06-29 11:31:33 -0300

    Implemented way to load services dynamically

--- config/services.php
+++ config/services.php
@@ -1,17 +1,32 @@
 <?php
 
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\Finder\Finder;
 
-$definition = new Definition();
+// Set composer vendor path.
+$composer_vendor = str_replace('/autoload.php', '', EDISONLABS_COMPOSER_INSTALL);
 
-$definition
-    ->setAutowired(true)
-    ->setAutoconfigured(true)
-    ->setPublic(true);
+// Find packages using the Metrics classes.
+$finder = new Finder();
+$finder->directories();
+$finder->followLinks();
+$finder->in("$composer_vendor/..");
+$finder->name('EdisonLabs');
 
-// $this is a reference to the current loader
-$this->registerClasses(
-    $definition,
-    'EdisonLabs\\Metrics\\',
-    '/home/jribeiro/Projects/git/composer/edison_metrics-8/src/EdisonLabs/Metrics/*'
-);
+// Register services.
+if ($finder->count() !== 0) {
+    $definition = new Definition();
+    $definition
+        ->setAutowired(true)
+        ->setAutoconfigured(true)
+        ->setPublic(true);
+
+    foreach ($finder as $folder) {
+        // $this is a reference to the current loader
+        $this->registerClasses(
+            $definition,
+            'EdisonLabs\\Metrics\\',
+            $folder->getRealPath() . '/Metrics'
+        );
+    }
+}
