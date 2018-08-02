5384d8265e7899498fa0d6d65a05905f1553691d
Manuel Garcia <manuel.drupal@gmail.com>
2018-08-02 13:35:59 +0100

    Refactor to use a mock instead of an annonymous class. No need to test the metric interface when the abstract class test covers it.

--- tests/src/Unit/MetricsTest.php
+++ tests/src/Unit/MetricsTest.php
@@ -70,69 +70,28 @@ class MetricsTest extends TestCase
      */
     public function testAbstractMetricBase()
     {
-        /**
-         * Anonymous class for testing AbstractMetricBase.
-         */
-        $abstractMetricBaseClass = new class() extends AbstractMetricBase
-        {
-            /**
-             * {@inheritdoc}
-             */
-            public function setConfig(array $config)
-            {
-                $this->config = $config;
-            }
+        $abstractMetricBaseMock = $this->getMockBuilder('EdisonLabs\Metrics\Metric\AbstractMetricBase')
+          ->setMethods(['getMetric', 'getDescription', 'getName'])
+          ->getMockForAbstractClass();
+        $abstractMetricBaseMock->expects($this->once())
+          ->method('getMetric')
+          ->willReturn(666);
+        $abstractMetricBaseMock->expects($this->once())
+          ->method('getDescription')
+          ->willReturn('Test metric description');
+        $abstractMetricBaseMock->expects($this->once())
+          ->method('getName')
+          ->willReturn('Test metric');
 
-            /**
-             * {@inheritdoc}
-             */
-            public function getConfig()
-            {
-                return parent::getConfig();
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function getGroups()
-            {
-                return parent::getGroups();
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function getName()
-            {
-                return 'Test metric';
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function getDescription()
-            {
-                return 'Test metric description';
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function getMetric()
-            {
-                return 666;
-            }
-
-        };
         $configuration = ['test' => '123'];
-        $abstractMetricBaseClass->setConfig($configuration);
-        $config = $abstractMetricBaseClass->getConfig();
+        $abstractMetricBaseMock->setConfig($configuration);
+        $config = $abstractMetricBaseMock->getConfig();
         $this->assertArrayHasKey('test', $config);
         $this->assertEquals($config['test'], $configuration['test']);
-        $this->assertEquals('Test metric', $abstractMetricBaseClass->getName());
-        $this->assertEquals('Test metric description', $abstractMetricBaseClass->getDescription());
-        $this->assertEquals(666, $abstractMetricBaseClass->getMetric());
-        $groups = $abstractMetricBaseClass->getGroups();
+        $this->assertEquals('Test metric', $abstractMetricBaseMock->getName());
+        $this->assertEquals('Test metric description', $abstractMetricBaseMock->getDescription());
+        $this->assertEquals(666, $abstractMetricBaseMock->getMetric());
+        $groups = $abstractMetricBaseMock->getGroups();
         $this->assertTrue(is_array($groups));
         $this->assertEmpty($groups);
     }
@@ -163,87 +122,6 @@ class MetricsTest extends TestCase
         $metric = $metricPercentage->getMetric();
         $this->assertNotNull($metric);
         $this->assertEquals(30, $metric);
-    }
-
-    /**
-     * Covers \EdisonLabs\Metrics\Metric\MetricInterface
-     *
-     * @return null
-     */
-    public function testMetricInterface()
-    {
-        /**
-         * Anonymous class for testing MetricInterface.
-         */
-        $metricInterfaceClass = new class() implements MetricInterface
-        {
-            /**
-             * The metrics config.
-             *
-             * @var array
-             */
-            protected $config;
-
-            /**
-             * {@inheritdoc}
-             */
-            public function setConfig(array $config)
-            {
-                $this->config = $config;
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function getConfig()
-            {
-                return $this->config;
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function getGroups()
-            {
-                return [];
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function getName()
-            {
-                return 'Test metric';
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function getDescription()
-            {
-                return 'Test metric description';
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function getMetric()
-            {
-                return 666;
-            }
-
-        };
-        $configuration = ['test' => '123'];
-        $metricInterfaceClass->setConfig($configuration);
-        $config = $metricInterfaceClass->getConfig();
-        $this->assertArrayHasKey('test', $config);
-        $this->assertEquals($config['test'], $configuration['test']);
-        $this->assertEquals('Test metric', $metricInterfaceClass->getName());
-        $this->assertEquals('Test metric description', $metricInterfaceClass->getDescription());
-        $this->assertEquals(666, $metricInterfaceClass->getMetric());
-        $groups = $metricInterfaceClass->getGroups();
-        $this->assertTrue(is_array($groups));
-        $this->assertEmpty($groups);
     }
 
     /**
